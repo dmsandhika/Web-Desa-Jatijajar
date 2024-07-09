07fbc161f6a43c75d0f1518de1ac38c492c195b7
Dimas Andhika Firmansyah <evandimas818@gmail.com>
2024-07-09 23:43:32 +0700

    update sintax view layanan

--- resources/views/surat/index.blade.php
+++ resources/views/surat/index.blade.php
@@ -1,23 +1,125 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot>
 
-  
+  @php
+   $data = [
+    [
+        'title' => 'Keterangan Belum Nikah',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Domisili',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Lahir',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Meninggal',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Tidak Mampu',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Usaha',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Kendaraan',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Pengantar Izin Keramaian',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Pengantar E-Ktp',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Pengantar SKCK',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Jual Beli',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Kehilangan',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Permohonan Bantuan Penunggu Pasien',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Penghasilan',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Surat Rekomendasi',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Penghasilan Orang Tua',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Permohonan Kuasa',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Perjanjian Kesepakatan',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Domisili Lembaga',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Pindah Domisili',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Ket. Beda Identitas',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Pengantar Catin',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Perkawinan',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Pernyataan Izin Keluarga',
+        'href' => '#',
+    ],
+    [
+        'title' => 'Keterangan Lainnya',
+        'href' => '#',
+    ],
+];
+ 
+  @endphp
 
-  <div class="grid gap-6 lg:grid-cols-4 sm:grid-cols-1">
-      
-    <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Surat Pengantar SKCK</h5>
-      </a>
-      <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-300 rounded-lg hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Ajukan Surat
-        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </a>
-    </div>
-
-
+  <div class="grid gap-6 lg:grid-cols-4 sm:grid-cols-2">
+    @foreach($data as $item)
+      <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="{{ $item['href'] }}">
+          <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $item['title'] }}</h5>
+        </a>
+        <a href="{{ $item['href'] }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-300 rounded-lg hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Ajukan Surat
+          <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+          </svg>
+        </a>
+      </div>
+    @endforeach
   </div>
-
 </x-layout>
